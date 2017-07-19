d1882fd2003af38bac53f7adf715fd8eb64c3bdc
xiaomagecdw <chendaiwu@chendaiwudeMacBook-Pro.local>
2017-07-20 01:00:40 +0800

    第一次上传

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from setuptools import setup
+
+PACKAGE = "pytest-allure-adaptor"
+VERSION = "1.7.7"
+
+install_requires = [
+    "lxml>=3.2.0",
+    "pytest>=2.7.3",
+    "namedlist",
+    "six>=1.9.0"
+]
+
+if sys.version_info < (3, 4):
+    install_requires.append("enum34")
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def main():
+    setup(
+        name=PACKAGE,
+        version=VERSION,
+        description=("Plugin for py.test to generate allure xml reports"),
+        author="pupssman",
+        author_email="pupssman@yandex-team.ru",
+        packages=["allure"],
+        url="https://github.com/allure-framework/allure-python",
+        long_description=read('README.rst'),
+        entry_points={'pytest11': ['allure_adaptor = allure.pytest_plugin']},
+        install_requires=install_requires
+    )
+
+
+if __name__ == '__main__':
+    main()
